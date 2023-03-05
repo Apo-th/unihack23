fe88d8d42e9acfcc2209492089c8f7f06ec3403e
KobeyRaff <kraf0002@student.monash.edu>
2023-03-05 12:46:30 +1100

    Update Sidebar.js
    ignore this lol

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,24 +1,16 @@
-import SidebarMenu from 'react-bootstrap-sidebar-menu';
+import React from 'react';
+import {Nav, Navbar, NavItem, Offcanvas, Container} from 'react-bootstrap';
 
-const Sidebar = () => {
+const Navbar = () => {
     return (
-        <SidebarMenu expand="sm" varient="dark" bg="dark">
-            <SidebarMenu.collapse>
-                <Sidebar.Header>
-                    <Sidebar.Brand>Logo</Sidebar.Brand>
-                    <Sidebar.Toggle />
-                </Sidebar.Header>
-                <SidebarMenu.Body>
-                    <SidebarMenu.Nav>
-                        <SidebarMenu.Nav.Link>
-                            <SidebarMenu.Nav.Icon>1</SidebarMenu.Nav.Icon>
-                            <SidebarMenu.Nav.Title>Menu Title</SidebarMenu.Nav.Title>
-                        </SidebarMenu.Nav.Link>
-                    </SidebarMenu.Nav>
-                </SidebarMenu.Body>
-                    
-            </SidebarMenu.collapse>
-        
-        </SidebarMenu>
+        <>
+        <Navbar bg="dark" variant="dark">
+            <Container fluid>
+                <Navbar.Brand href="#home">Ca$h Cam</Navbar.Brand>
+                <Navbar.Toggle aria-controls="offcanvasNavbar" />
+            </Container>
+                
+        </Navbar>
+        </>
     );
 }
