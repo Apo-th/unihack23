2a870bf9623efb0d7b5c54a381dba804784c2941
Daniel Ding <ddin0018@student.monash.edu>
2023-03-05 15:39:22 +1100

    camera functional

--- app/django_react_proj/students/serializers.py
+++ app/django_react_proj/students/serializers.py
@@ -1,17 +1,10 @@
 from rest_framework import serializers
-<<<<<<< HEAD
-from .models import Student
-=======
 from .models import Student, Receipt
->>>>>>> develop
 
 class StudentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Student 
-<<<<<<< HEAD
-        fields = ('pk', 'name', 'email', 'document', 'phone', 'registrationDate')
-=======
         fields = ('pk', 'name', 'email', 'document', 'phone', 'registrationDate')
 
 class ReceiptSerializer(serializers.ModelSerializer):
@@ -19,4 +12,3 @@ class ReceiptSerializer(serializers.ModelSerializer):
     class Meta:
         model = Receipt 
         fields = ('name', 'receipt_img')
->>>>>>> develop
