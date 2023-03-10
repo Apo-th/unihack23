7e0c9ad551fbfed3475273f9164414bc247117a7
Daniel Ding <ddin0018@student.monash.edu>
2023-03-11 02:40:04 +1100

    Chart.js added in

--- views/my-app/src/Home.js
+++ views/my-app/src/Home.js
@@ -1,8 +1,6 @@
-import React, { Component } from "react";
-import StudentList from './StudentList';
 import axios from "axios";
-import { API_URL, API_URL_SPEND } from ".//constants";
-import { useState } from "react";
+import { API_URL_SPEND } from ".//constants";
+import { useEffect, useState } from "react";
 import { Form, Button, Card } from 'react-bootstrap';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js/auto";
 import { Bar } from "react-chartjs-2";
@@ -11,12 +9,37 @@ import { Bar } from "react-chartjs-2";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Home = () => {
+  // Hooks
   const [receipts, setReceipts] = useState(null)
+  const merchantMap = new Map();
+  const categoryMap = new Map();
+  const descriptionMap = new Map();
+  const [graphLabels, setLabels] = useState([]);
+  const [graphData, setGraphData] = useState([]);
 
-  axios.get(API_URL_SPEND).then(function (response) {
-    console.log(response.data)
-  }
-  )
+  // GET Request
+  const getSpending = () => {
+    axios.get(API_URL_SPEND).then(function (response) {
+      // Map of categories, merchants and descriptions
+      response.data.forEach(element => {
+        merchantMap.set(element.merchant, 1);
+        categoryMap.set(element.category, 1);
+        descriptionMap.set(element.description, element.total_price);
+      }
+      )
+
+    }
+    ).then(() => {
+      const arr = [];
+      const costs = [];
+      descriptionMap.forEach((value, key, map) => {
+        arr.push(key);
+        costs.push(value);
+      });
+      setLabels(arr);
+      setGraphData(costs);
+    })
+  };
 
   const options = {
     responsive: true,
@@ -31,12 +54,12 @@ const Home = () => {
     },
   };
 
-  const labels = ['Jan','Feb','Mar','Apr','May','Jun','Jul'];
+  const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'];
   const data = {
-    labels: labels,
+    labels: graphLabels,
     datasets: [{
       label: 'My First Dataset',
-      data: [65, 59, 80, 81, 56, 55, 40],
+      data: graphData,
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(255, 159, 64, 0.2)',
@@ -59,17 +82,18 @@ const Home = () => {
     }]
   };
 
+  useEffect(() => {
+    getSpending();
+  })
 
   return (
-    <Card className="bg-dark text-white advisor-card" variant="dark">
-      <Card.Body>
-        <Card.Title>A glance at your financials:</Card.Title>
-        <Bar
-        options = {options}
-          data={data}
-       />
-      </Card.Body>
-    </Card>
+    // <Card className="bg-dark text-white advisor-card" variant="dark">
+    //   <Card.Title>A glance at your financials:</Card.Title>
+    //   <Card.Body>
+      <Bar options={options} data={data}/>
+    //   </Card.Body>
+    // </Card>
+
   );
 }
 
